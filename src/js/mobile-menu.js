107e6df39b4d4475126a67d7544e770c8dab4e3d
MikoreVikt <104897659+MikoreVikt@users.noreply.github.com>
2022-08-11 01:25:04 +0300

    adds fixes to mobile-menu.js

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,20 +1,22 @@
 (() => {
-  const menuBtnRef = document.querySelector("[data-menu-button]");
-  const mobileMenuRef = document.querySelector("[data-menu]");
-  const linksRef = [document.getElementsByClassName("mobile-menu-nav__link")];
+  const menuBtnRef = document.querySelector('[data-menu-button]');
+  const mobileMenuRef = document.querySelector('[data-menu]');
+  const linksRef = document.getElementsByClassName('mobile-menu-nav__link');
   const bodyRef = document.body;
-  menuBtnRef.addEventListener("click", openCloseMenu);
+  menuBtnRef.addEventListener('click', openCloseMenu);
 
-  linksRef.forEach(element => element.addEventListener("click", openCloseMenu));
+  for (let i = 0; i < linksRef.length; i++) {
+    linksRef[i].addEventListener('click', openCloseMenu);
+  }
 
   function openCloseMenu() {
     const expanded =
-      menuBtnRef.getAttribute("aria-expanded") === "true" || false;
+      menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
 
-    menuBtnRef.classList.toggle("is-open");
-    menuBtnRef.setAttribute("aria-expanded", !expanded);
+    menuBtnRef.classList.toggle('is-open');
+    menuBtnRef.setAttribute('aria-expanded', !expanded);
 
-    mobileMenuRef.classList.toggle("is-open");
-    bodyRef.classList.toggle("no-scroll");
+    mobileMenuRef.classList.toggle('is-open');
+    bodyRef.classList.toggle('no-scroll');
   }
 })();

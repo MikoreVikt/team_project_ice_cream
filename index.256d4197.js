4993cf456cd0dc3208f86e64779616377ed2c3ca
MikoreVikt <104897659+MikoreVikt@users.noreply.github.com>
2022-08-12 09:17:28 +0000

    Deploying to gh-pages from @ MikoreVikt/team_project_ice_cream@2ddf322a4afd04128c4ce807e6eadc4c25ab606b 🚀

--- index.256d4197.js
+++
@@ -1,2 +0,0 @@
-(()=>{const e=document.querySelector("[data-menu-button]"),t=document.querySelector("[data-menu]"),n=document.getElementsByClassName("mobile-menu-nav__link"),o=document.body;e.addEventListener("click",s);for(let e=0;e<n.length;e++)n[e].addEventListener("click",s);function s(){const n="true"===e.getAttribute("aria-expanded")||!1;e.classList.toggle("is-open"),e.setAttribute("aria-expanded",!n),t.classList.toggle("is-open"),o.classList.toggle("no-scroll")}})();
-//# sourceMappingURL=index.256d4197.js.map

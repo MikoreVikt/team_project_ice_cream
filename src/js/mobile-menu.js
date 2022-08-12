102bb25eb3637bf4767be250768320762fc9999c
OleksiiChernii <oleksii.chernii@gmail.com>
2022-08-12 14:01:31 +0300

    top botton appearance

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -20,3 +20,16 @@
     bodyRef.classList.toggle('no-scroll');
   }
 })();
+
+
+mybutton = document.getElementById("myBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}

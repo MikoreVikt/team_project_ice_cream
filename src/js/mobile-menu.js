09c1a34c8172b6ccd3ac51d5a7e2a1b511c68e97
OleksiiChernii <oleksii.chernii@gmail.com>
2022-08-12 14:02:29 +0300

    smooth scrolling

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -33,3 +33,14 @@ function scrollFunction() {
     mybutton.style.display = "none";
   }
 }
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+          behavior: 'smooth'
+      });
+  });
+});
+

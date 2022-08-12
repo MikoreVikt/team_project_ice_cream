05492af7923ed65457476a207bfae74b69b40f58
MikoreVikt <104897659+MikoreVikt@users.noreply.github.com>
2022-08-12 17:11:32 +0000

    Deploying to gh-pages from @ MikoreVikt/team_project_ice_cream@db516e150ada8554faa853d898a1c279f7c00aec 🚀

---
+++ index.15d60d4d.js
@@ -0,0 +1,2 @@
+mybutton=document.getElementById("myBtn"),window.onscroll=function(){document.body.scrollTop>20||document.documentElement.scrollTop>20?mybutton.style.display="block":mybutton.style.display="none"},document.querySelectorAll('a[href^="#"]').forEach((t=>{t.addEventListener("click",(function(t){t.preventDefault(),document.querySelector(this.getAttribute("href")).scrollIntoView({behavior:"smooth"})}))}));
+//# sourceMappingURL=index.15d60d4d.js.map
